d1327eb21aaa531ae3dafbe79516a55eb9596e3c
nBrzoza <nbrzoza@pm.me>
2023-07-13 21:51:30 +0200

    Fixed #my_projects

--- project_details.php
+++ project_details.php
@@ -5,19 +5,14 @@
   $results = pg_fetch_object($query);
 
   if ($results) {
-<<<<<<< Updated upstream
-    echo "
-        Name: ".$results ->name."
-=======
     echo "<div class='project-details'><h2><b>Name: </b> ".$results->name."</h2>
-      <a href='".$results->github_link."' target='_blank'><img src='github.png' style='height: 1.5em;width: 1.5em;background: white;border: 2px solid white;border-radius: 100%;'> </a>
-      <a href='https://".$results->demo_url."' target='_blank'> DEMO </a>
       <p>".$results->description."</p>
       <h3>Technologies I used:</h3>
         ".$results->technologies_name."<br><br>
         ".$results->libraries_name."
       </div>
->>>>>>> Stashed changes
+      <a href='".$results->github_link."' target='_blank'><img src='github.png' style='height: 1.5em;width: 1.5em;background: white;border: 2px solid white;border-radius: 100%;'> </a>
+      <a href='https://".$results->demo_url."' target='_blank'> DEMO </a>
     ";
   } else {
     echo "Something went wrong!";
