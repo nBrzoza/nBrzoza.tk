f01592f4327151dbc4293ba9bbd0dbb62c4d7d18
nBrzoza <nbrzoza@pm.me>
2023-07-15 15:55:25 +0200

    New README.md file

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta 
+      name="description" 
+      content="Hi! My name is Norbert and this is my portfolio! Check it out to find more about me!">
     <title>nBrzoza</title>
   </head>
   <body>
